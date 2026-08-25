6f2c84fed12503cf53f56d4ef2e6ef47bc110593
Konrad Ołdakowski <konrad.oldakowski03@gmail.com>
2026-08-25 22:31:06 +0200

    chore: initialize Spring Boot project

---
+++ library-api/src/test/java/pl/konradoldakowski/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.konradoldakowski.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mock;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
