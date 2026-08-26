787e16659aafa54a9fac375c14dd3aafba05db0a
Konrad Ołdakowski <konrad.oldakowski03@gmail.com>
2026-08-26 19:20:45 +0200

    feat: add Book entity

---
+++ library-api/src/main/java/pl/konradoldakowski/libraryapi/Book.java
@@ -0,0 +1,58 @@
+package pl.konradoldakowski.libraryapi;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    private String author;
+    @Column(name = "publication_year")
+    private int publicationYear;
+    private String isbn;
+
+    public Book() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
