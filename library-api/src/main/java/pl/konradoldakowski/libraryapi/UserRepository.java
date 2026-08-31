9208b9766925e0df54ada0660b96b3548dcbc508
Konrad Ołdakowski <konrad.oldakowski03@gmail.com>
2026-08-31 20:29:13 +0200

    feat: Implemented methods to delete user and to update user in UserService

--- library-api/src/main/java/pl/konradoldakowski/libraryapi/UserRepository.java
+++ library-api/src/main/java/pl/konradoldakowski/libraryapi/UserRepository.java
@@ -2,6 +2,9 @@ package pl.konradoldakowski.libraryapi;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
--- library-api/src/main/java/pl/konradoldakowski/libraryapi/UserService.java
+++ library-api/src/main/java/pl/konradoldakowski/libraryapi/UserService.java
@@ -4,6 +4,7 @@ package pl.konradoldakowski.libraryapi;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.StreamSupport;
 
 @Service
@@ -26,5 +27,22 @@ public class UserService {
         Iterable<User> all = userRepository.findAll();
         return StreamSupport.stream(all.spliterator(), false).toList();
     }
-
+    public void deleteUserById(Long id) {
+        if(!userRepository.existsById(id)) {
+            throw new UserNotFoundException("User with id " + id + " not found");
+        }
+        userRepository.deleteById(id);
+    }
+    public User updateUser(Long id, User user) {
+        User userToUpdate = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User with id " + id + " not found"));
+        Optional<User> userByEmail = userRepository.findByEmail(user.getEmail());
+        if(userByEmail.isPresent() && !userByEmail.get().getId().equals(id)) {
+            throw new EmailAlreadyInUseException("User with given email already exists");
+        }
+        userToUpdate.setFirstName(user.getFirstName());
+        userToUpdate.setLastName(user.getLastName());
+        userToUpdate.setEmail(user.getEmail());
+        userToUpdate.setPhoneNumber(user.getPhoneNumber());
+        return userRepository.save(userToUpdate);
+    }
 }
