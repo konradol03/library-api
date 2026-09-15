bf3a7de01631b92ce0adc8881f3f94871ebe61b4
Konrad Ołdakowski <konrad.oldakowski03@gmail.com>
2026-09-15 16:47:56 +0200

    feat: add AuthenticationManager

--- library-api/src/main/java/pl/konradoldakowski/libraryapi/security/SecurityConfig.java
+++ library-api/src/main/java/pl/konradoldakowski/libraryapi/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package pl.konradoldakowski.libraryapi.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +15,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
