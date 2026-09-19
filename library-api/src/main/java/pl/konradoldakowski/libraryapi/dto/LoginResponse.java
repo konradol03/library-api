ad27a9d5e64e6ff3a70fc959b40f6a21b592d4f1
Konrad Ołdakowski <konrad.oldakowski03@gmail.com>
2026-09-19 20:32:45 +0200

    feat: add LoginResponse DTO

---
+++ library-api/src/main/java/pl/konradoldakowski/libraryapi/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package pl.konradoldakowski.libraryapi.dto;
+
+public class LoginResponse {
+    private final String token;
+
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+}
