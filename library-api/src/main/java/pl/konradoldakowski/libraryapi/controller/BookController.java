06a5d47233f123e9c20ab2196b99c3a809c50c67
Konrad Ołdakowski <konrad.oldakowski03@gmail.com>
2026-09-22 16:22:15 +0200

    feat: add authorization to Book endpoints

--- library-api/src/main/java/pl/konradoldakowski/libraryapi/controller/BookController.java
+++ library-api/src/main/java/pl/konradoldakowski/libraryapi/controller/BookController.java
@@ -2,6 +2,7 @@ package pl.konradoldakowski.libraryapi.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.konradoldakowski.libraryapi.dto.BookResponse;
@@ -28,17 +29,21 @@ public class BookController {
     public ResponseEntity<List<BookResponse>> getAllBooks(){
         return ResponseEntity.ok(bookService.getAllBooks());
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<Book> createBook(@Valid @RequestBody Book book){
         Book createdBook = bookService.createBook(book);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdBook.getId()).toUri();
         return ResponseEntity.created(location).body(createdBook);
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
         return ResponseEntity.noContent().build();
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateBook(@PathVariable Long id,@Valid @RequestBody Book book){
         return ResponseEntity.ok(bookService.updateBook(id, book));
--- library-api/src/main/java/pl/konradoldakowski/libraryapi/security/SecurityConfig.java
+++ library-api/src/main/java/pl/konradoldakowski/libraryapi/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,7 @@ import pl.konradoldakowski.libraryapi.service.CustomUserDetailsService;
 import pl.konradoldakowski.libraryapi.service.JwtService;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
     private final JwtService jwtService;
     private final CustomUserDetailsService customUserDetailsService;
@@ -41,8 +43,8 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(exception -> exception
-                        .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-                        .accessDeniedHandler((request, response, accessDeniedException) -> response.sendError(HttpServletResponse.SC_FORBIDDEN)))
+                        .authenticationEntryPoint((request, response, authException) -> response.setStatus(HttpServletResponse.SC_UNAUTHORIZED))
+                        .accessDeniedHandler((request, response, accessDeniedException) -> response.setStatus(HttpServletResponse.SC_FORBIDDEN)))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/register","/auth/login").permitAll().anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
