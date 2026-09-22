a2c60c5f649bbbb510782aabaebad19490e0cec4
Konrad Ołdakowski <konrad.oldakowski03@gmail.com>
2026-09-22 15:21:09 +0200

    feat: add JWT authentication filter and stateless session management

--- library-api/src/main/java/pl/konradoldakowski/libraryapi/security/SecurityConfig.java
+++ library-api/src/main/java/pl/konradoldakowski/libraryapi/security/SecurityConfig.java
@@ -6,12 +6,24 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import pl.konradoldakowski.libraryapi.service.CustomUserDetailsService;
+import pl.konradoldakowski.libraryapi.service.JwtService;
 
 @Configuration
 public class SecurityConfig {
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfig(JwtService jwtService, CustomUserDetailsService customUserDetailsService) {
+        this.jwtService = jwtService;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -24,8 +36,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationFilter jwtFilter = new JwtAuthenticationFilter(jwtService, customUserDetailsService);
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/register","/auth/login").permitAll().anyRequest().authenticated());
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/register","/auth/login").permitAll().anyRequest().authenticated())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
